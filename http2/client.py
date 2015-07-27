26f054cf8fe003a83082f5eab672d54449cd34fe
Luavis <luaviskang@gmail.com>
2015-07-28 05:00:50 +0900

    Remove comment to use it 1.1 client

--- http2/client.py
+++ http2/client.py
@@ -1,22 +1,21 @@
 from socket import socket, AF_INET, SOCK_STREAM
-from frame import Frame
 
 BUFF_SIZE = 1024
 ADDR = ('localhost', 8080)
 
 
-def main():
+def main():  # Testing HTTP/1.1
 
     client = socket(AF_INET, SOCK_STREAM)
 
     client.connect(ADDR)
 
-    # client.send('GET / HTTP/1.1\r\n\r\n')
+    client.send('GET / HTTP/1.1\r\n\r\n')
 
-    # while True:
+    while True:
 
-    #     data = client.recv(BUFF_SIZE)
-    #     print(data)
+        data = client.recv(BUFF_SIZE)
+        print(data)
 
 if __name__ == '__main__':
     main()
