7e9136fe87e09ba73ec5d0482a88a049318a9ed7
Luavis <luaviskang@gmail.com>
2015-08-02 01:34:06 +0900

    Add HEADERS frame test

--- tests/test_header_frame.py
+++ tests/test_header_frame.py
@@ -7,20 +7,78 @@
 """
 
 from http2.header_frame import HeaderFrame
+from http2.frame import (Frame, FrameType)
 
 
 def test_header_frame_data():
 
     frame = HeaderFrame(id=0x0201)
 
-    frame.method("GET")
+    frame.method = "GET"
 
-    frame.scheme('https')
+    frame.scheme = 'https'
 
-    frame.path('/')
+    frame.path = '/'
 
-    frame.add('host', 'locahost')
+    frame.add('host', 'localhost')
 
     frame.add('accept', 'text/html')
 
-    frame.get_frame_bin()
+    assert frame.type == FrameType.HEADERS
+
+    read_frame = Frame.load(frame.get_frame_bin())
+
+    assert read_frame.method == "GET"
+
+    assert read_frame.scheme == "https"
+
+    assert read_frame.path == "/"
+
+    assert read_frame.get('host') == "localhost"
+
+    assert read_frame.get('accept') == "text/html"
+
+
+def test_multiple_header_frame_data():
+
+    encoded_frames = []
+
+    frame = HeaderFrame(id=0x0201, end_header=False)
+
+    frame.method = "GET"
+
+    frame.scheme = 'https'
+
+    frame.path = '/'
+
+    assert frame.type == FrameType.HEADERS
+
+    encoded_frames.append(Frame.load(frame.get_frame_bin()))
+
+    assert encoded_frames[0].is_encoded
+
+    second_frame = HeaderFrame(id=0x0201, end_header=False)
+
+    second_frame.add('host', 'localhost')
+
+    second_frame.add('accept', 'text/html')
+
+    encoded_frames.append(Frame.load(second_frame.get_frame_bin()))
+
+    last_frame = HeaderFrame(id=0x0201, end_header=True)
+
+    last_frame.add('accept-language', 'ko')
+
+    read_frame = Frame.load(last_frame.get_frame_bin(), None, encoded_frames)
+
+    assert read_frame.method == "GET"
+
+    assert read_frame.scheme == "https"
+
+    assert read_frame.path == "/"
+
+    assert read_frame.get('host') == "localhost"
+
+    assert read_frame.get('accept') == "text/html"
+
+    assert read_frame.get('accept-language') == "ko"
